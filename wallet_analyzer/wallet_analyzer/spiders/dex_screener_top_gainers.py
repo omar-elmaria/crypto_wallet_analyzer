7aec54cd1788b4bfd497d1b9de620ea2747c06b1
Omar ElMaria <omar.elmaria@mim2018.esmt.org>
2025-01-02 23:56:18 +0200

    separate the spider

--- wallet_analyzer/wallet_analyzer/spiders/dex_screener_top_gainers.py
+++ wallet_analyzer/wallet_analyzer/spiders/dex_screener_top_gainers.py
@@ -2,22 +2,17 @@
 import scrapy
 from inputs import custom_scrapy_settings
 from helper_functions import *
-from wallet_analyzer.items import DexScreenerTopGainers, DexScreenerTopTraders
 import re
 
-class DexScreenerCrawlerSpider(scrapy.Spider):
-    name = "dex_screener_crawler"
-    custom_settings = custom_scrapy_settings # Define the custom settings of the spider
+
+class DexScreenerTopGainersSpider(scrapy.Spider):
+    name = "dex_screener_top_gainers"
+    custom_settings = custom_scrapy_settings.copy() # Define the custom settings of the spider
+    custom_settings["LOG_FILE"] = "dex_screener_top_gainers.log"
     custom_settings["FEEDS"] = {
         'dex_screener_top_gainers.json': {
             'format': 'json',
-            'overwrite': True,
-            'item_classes': ["wallet_analyzer.items.DexScreenerTopGainers"]
-        },
-        'dex_screener_top_traders.json': {
-            'format': 'json',
-            'overwrite': True,
-            'item_classes': ["wallet_analyzer.items.DexScreenerTopTraders"]
+            'overwrite': True
         }
     }
     base_url = "https://dexscreener.com/gainers/solana?min24HSells=30&min24HTxns=300&min24HVol=500000&minLiq=250000&minMarketCap=1000000&order=desc&rankBy=priceChangeH24" # Volume > 500k, Liquidity > 250k, MCap > 1M
@@ -25,6 +20,7 @@ class DexScreenerCrawlerSpider(scrapy.Spider):
     ## Start scraping
     def start_requests(self):
         # Send a request to the base URL
+        self.logger.info("Sending a request to the base URL")
         yield scrapy.Request(
             url=self.base_url,
             callback=self.parse_top_gainers,
@@ -36,14 +32,14 @@ class DexScreenerCrawlerSpider(scrapy.Spider):
         )
 
     def parse_top_gainers(self, response):
+        # Log a status message
+        self.logger.info("Parsing the response from the base URL")
+
         # Extract a list of results
         results = response.xpath("//div[@class='ds-dex-table ds-dex-table-top']/a")
 
-        # Create an instance of DexScreenerTopGainers
-        top_gainers_item_obj = DexScreenerTopGainers()
-
         # Parse the response
-        for res in results[0:2]:
+        for res in results:
             # Extract the asset name
             asset_name = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-token']/span[contains(@class, 'ds-dex-table-row-base-token-symbol')]/text()").get()
             
@@ -105,105 +101,24 @@ class DexScreenerCrawlerSpider(scrapy.Spider):
             asset_market_cap_in_mil = helper_normalize_millions_and_thousands_in_vol_liq_mcap(value=asset_market_cap_in_mil)
 
             # Yield the output dictionary
-            for var in [
-                "asset_name", "asset_name_text", "asset_url",
-                "asset_gain_rank", "asset_network", "dex",
-                "asset_price", "asset_age", "asset_24_hr_txns",
-                "asset_24_hr_volume_in_mil", "num_makers", "asset_price_change_l5m",
-                "asset_price_change_l1h", "asset_price_change_l6h", "asset_price_change_l24h",
-                "asset_liquidity_in_mil", "asset_market_cap_in_mil"
-            ]:
-                top_gainers_item_obj[var] = eval(var)
+            output_dict = {
+                "asset_name": asset_name,
+                "asset_name_text": asset_name_text,
+                "asset_url": asset_url,
+                "asset_gain_rank": asset_gain_rank,
+                "asset_network": asset_network,
+                "dex": dex,
+                "asset_price": asset_price,
+                "asset_age": asset_age,
+                "asset_24_hr_txns": asset_24_hr_txns,
+                "asset_24_hr_volume_in_mil": asset_24_hr_volume_in_mil,
+                "num_makers": num_makers,
+                "asset_price_change_l5m": asset_price_change_l5m,
+                "asset_price_change_l1h": asset_price_change_l1h,
+                "asset_price_change_l6h": asset_price_change_l6h,
+                "asset_price_change_l24h": asset_price_change_l24h,
+                "asset_liquidity_in_mil": asset_liquidity_in_mil,
+                "asset_market_cap_in_mil": asset_market_cap_in_mil
+            }
 
-            yield top_gainers_item_obj
-        
-            # Send a request to the individual asset URL
-            yield scrapy.Request(
-                url=asset_url,
-                callback=self.parse_top_traders,
-                meta={
-                    "zyte_api_automap": {
-                        "browserHtml": True,
-                        "javascript": True,
-                        "actions": [
-                            # Wait for the Top Traders Button
-                            {
-                                "action": "waitForSelector",
-                                "timeout": 10,
-                                "onError": "return",
-                                "selector": {
-                                    "type": "xpath",
-                                    "value": "//button[text() = 'Top Traders']",
-                                    "state": "attached"
-                                }
-                            },
-                            # Click on the Top Traders Button
-                            {
-                                "action": "click",
-                                "delay": 0,
-                                "button": "left",
-                                "onError": "return",
-                                "selector": {
-                                    "type": "xpath",
-                                    "value": "//button[text() = 'Top Traders']",
-                                    "state": "attached"
-                                }
-                            },
-                        ]
-                    },
-
-                    # Meta data
-                    "asset_name": asset_name,
-                    "asset_url": asset_url
-                }
-            )
-
-    def parse_top_traders(self, response):
-        # Create an instance of DexScreenerTopTraders
-        top_traders_item_obj = DexScreenerTopTraders()
-
-        # Extract the top traders list of results
-        top_trader_results = response.xpath("//span[text() = 'bought']/../../following-sibling::div")
-
-        # Extract the meta data
-        asset_name = response.meta["asset_name"]
-        asset_url = response.meta["asset_url"]
-
-        # Parse the response
-        for tr in top_trader_results:
-            # Extract the trader bought amount in USD
-            trader_bought_usd = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/text()").get()
-
-            # Extract the trader bought amount in crypto units
-            trader_bought_crypto = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/following-sibling::span/span[1]/text()").get()
-            
-            # Extract the number of buy TXNs
-            trader_buy_txns = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/following-sibling::span/span[3]/text()").get()
-
-            # Extract the trader sold amount in USD
-            trader_sold_usd = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/text()").get()
-            
-            # Extract the trader sold amount in crypto units
-            trader_sold_crypto = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/following-sibling::span/span[1]/text()").get()
-            
-            # Extract the number of sell TXNs
-            trader_sell_txns = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/following-sibling::span/span[3]/text()").get()
-            
-            # Extract the PnL
-            trader_pnl = tr.xpath(".//div[@class='custom-1e9y0rl']/text()").get()
-
-            # Extract the SOL scan URL
-            sol_scan_url = tr.xpath(".//a[@aria-label='Open in block explorer']/@href").get()
-
-            # Extract the wallet_address
-            wallet_address = re.findall(pattern=r"(?<=account\/).*", string=sol_scan_url)[0]
-
-            # Yield the output dictionary
-            for var in [
-                "asset_name", "asset_url", "trader_bought_usd", "trader_bought_crypto", "trader_buy_txns",
-                "trader_sold_usd", "trader_sold_crypto", "trader_sell_txns",
-                "trader_pnl", "sol_scan_url", "wallet_address"
-            ]:
-                top_traders_item_obj[var] = eval(var)
-
-            yield top_traders_item_obj
+            yield output_dict
---
+++ wallet_analyzer/wallet_analyzer/spiders/dex_screener_top_traders.py
@@ -0,0 +1,130 @@
+# Import libraries
+import scrapy
+from inputs import custom_scrapy_settings
+from helper_functions import *
+import re
+import json
+import pandas as pd
+
+
+class DexScreenerTopTradersSpider(scrapy.Spider):
+    name = "dex_screener_top_traders"
+    custom_settings = custom_scrapy_settings.copy() # Define the custom settings of the spider
+    custom_settings["LOG_FILE"] = "dex_screener_top_traders.log"
+    custom_settings["FEEDS"] = {
+        'dex_screener_top_traders.json': {
+            'format': 'json',
+            'overwrite': True
+        }
+    }
+    
+    def start_requests(self):
+        # Open the JSON file dex_screener_top_gainers.json
+        self.logger.info("Opening the JSON file dex_screener_top_gainers.json")
+        with open("dex_screener_top_gainers.json", "r") as f:
+            # Load the JSON file
+            df_top_gainers = json.load(f)
+        
+            # Change the top_gainers to a pandas data frame
+            df_top_gainers = pd.DataFrame(df_top_gainers)
+
+        # Send a request to each individual asset URL
+        for asset_name, asset_url in zip(df_top_gainers["asset_name"], df_top_gainers["asset_url"]):
+            # Send a request to the asset URL
+            self.logger.info(f"Sending a request to the asset name {asset_name} with URL: {asset_url}")
+            yield scrapy.Request(
+                url=asset_url,
+                callback=self.parse_top_traders,
+                meta={
+                    "zyte_api_automap": {
+                        "browserHtml": True,
+                        "javascript": True,
+                        "actions": [
+                            # Wait for the Top Traders Button
+                            {
+                                "action": "waitForSelector",
+                                "timeout": 10,
+                                "onError": "return",
+                                "selector": {
+                                    "type": "xpath",
+                                    "value": "//button[text() = 'Top Traders']",
+                                    "state": "attached"
+                                }
+                            },
+                            # Click on the Top Traders Button
+                            {
+                                "action": "click",
+                                "delay": 0,
+                                "button": "left",
+                                "onError": "return",
+                                "selector": {
+                                    "type": "xpath",
+                                    "value": "//button[text() = 'Top Traders']",
+                                    "state": "attached"
+                                }
+                            },
+                        ]
+                    },
+
+                    # Meta data
+                    "asset_name": asset_name,
+                    "asset_url": asset_url
+                }
+            )
+
+    def parse_top_traders(self, response):
+        # Log a status message
+        self.logger.info(f"Parsing the response of the asset {response.meta['asset_name']} with URL {response.meta['asset_url']}")
+        
+        # Extract the top traders list of results
+        top_trader_results = response.xpath("//span[text() = 'bought']/../../following-sibling::div")
+
+        # Extract the meta data
+        asset_name = response.meta["asset_name"]
+        asset_url = response.meta["asset_url"]
+
+        # Parse the response
+        for tr in top_trader_results:
+            # Extract the trader bought amount in USD
+            trader_bought_usd = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/text()").get()
+
+            # Extract the trader bought amount in crypto units
+            trader_bought_crypto = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/following-sibling::span/span[1]/text()").get()
+            
+            # Extract the number of buy TXNs
+            trader_buy_txns = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/following-sibling::span/span[3]/text()").get()
+
+            # Extract the trader sold amount in USD
+            trader_sold_usd = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/text()").get()
+            
+            # Extract the trader sold amount in crypto units
+            trader_sold_crypto = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/following-sibling::span/span[1]/text()").get()
+            
+            # Extract the number of sell TXNs
+            trader_sell_txns = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/following-sibling::span/span[3]/text()").get()
+            
+            # Extract the PnL
+            trader_pnl = tr.xpath(".//div[@class='custom-1e9y0rl']/text()").get()
+
+            # Extract the SOL scan URL
+            sol_scan_url = tr.xpath(".//a[@aria-label='Open in block explorer']/@href").get()
+
+            # Extract the wallet_address
+            wallet_address = re.findall(pattern=r"(?<=account\/).*", string=sol_scan_url)[0]
+
+            # Yield the output dictionary
+            output_dict = {
+                "asset_name": asset_name,
+                "asset_url": asset_url,
+                "trader_bought_usd": trader_bought_usd,
+                "trader_bought_crypto": trader_bought_crypto,
+                "trader_buy_txns": trader_buy_txns,
+                "trader_sold_usd": trader_sold_usd,
+                "trader_sold_crypto": trader_sold_crypto,
+                "trader_sell_txns": trader_sell_txns,
+                "trader_pnl": trader_pnl,
+                "sol_scan_url": sol_scan_url,
+                "wallet_address": wallet_address
+            }
+
+            yield output_dict
