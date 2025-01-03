7da2393bf0a64f574b3fa222da9bca931efd1d77
Omar ElMaria <omar.elmaria@mim2018.esmt.org>
2025-01-03 14:55:31 +0200

    add raw cols to top gainers

--- wallet_analyzer/wallet_analyzer/spiders/dex_screener_top_gainers.py
+++ wallet_analyzer/wallet_analyzer/spiders/dex_screener_top_gainers.py
@@ -50,7 +50,8 @@ class DexScreenerTopGainersSpider(scrapy.Spider):
             asset_url = "https://dexscreener.com" + res.xpath("./@href").get()
 
             # Extract the 24-hour gain rank
-            asset_gain_rank = helper_treat_none_before_data_type_change(value=res.xpath(".//span[@class='ds-dex-table-row-badge-pair-no']/text()[2]").get(), data_type="int")
+            asset_gain_rank_raw = res.xpath(".//span[@class='ds-dex-table-row-badge-pair-no']/text()[2]").get()
+            asset_gain_rank = helper_treat_none_before_data_type_change(value=asset_gain_rank_raw, data_type="int")
             
             # Extract the network
             asset_network = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-token']/img[@class='ds-dex-table-row-chain-icon']/@title").get()
@@ -59,65 +60,77 @@ class DexScreenerTopGainersSpider(scrapy.Spider):
             dex = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-token']/img[@class='ds-dex-table-row-dex-icon']/@title").get()
             
             # Extract the latest price in dollars
-            asset_price = helper_treat_none_before_data_type_change(value=res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price']/text()[2]").get(), data_type="float")
+            asset_price_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price']/text()[2]").get()
+            asset_price = helper_treat_none_before_data_type_change(value=asset_price_raw, data_type="float")
             
             # Extract the asset age in hours
             asset_age = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-pair-age']/span/text()").get()
             
             # Extract the asset's number of transactions in the last 24 hours
-            asset_24_hr_txns = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-txns']/text()").get()
-            asset_24_hr_txns = helper_treat_none_before_data_type_change(value=re.sub(pattern=",", repl="", string=asset_24_hr_txns), data_type="int")
+            asset_24_hr_txns_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-txns']/text()").get()
+            asset_24_hr_txns = helper_treat_none_before_data_type_change(value=re.sub(pattern=",", repl="", string=asset_24_hr_txns_raw), data_type="int")
             
             # Extract the asset's volume in the last 24 hours
-            asset_24_hr_volume_in_mil = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-volume']/text()[2]").get()
-            asset_24_hr_volume_in_mil = helper_normalize_numbers_in_vol_liq_mcap(value=asset_24_hr_volume_in_mil)
+            asset_24_hr_volume_in_mil_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-volume']/text()[2]").get()
+            asset_24_hr_volume_in_mil = helper_normalize_numbers_in_vol_liq_mcap(value=asset_24_hr_volume_in_mil_raw)
             
             # Extract the asset's volume in the last 24 hours
-            num_makers = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-makers']/text()").get()
-            num_makers = helper_treat_none_before_data_type_change(value=re.sub(pattern=",", repl="", string=num_makers), data_type="int")
+            num_makers_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-makers']/text()").get()
+            num_makers = helper_treat_none_before_data_type_change(value=re.sub(pattern=",", repl="", string=num_makers_raw), data_type="int")
             
             # Extract the asset's price change in the last 5 minutes
-            asset_price_change_l5m = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price-change-m5']/span/text()").get()
-            asset_price_change_l5m = helper_normalize_numbers_in_pct_gains(value=asset_price_change_l5m)
+            asset_price_change_l5m_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price-change-m5']/span/text()").get()
+            asset_price_change_l5m = helper_normalize_numbers_in_pct_gains(value=asset_price_change_l5m_raw)
             
             # Extract the asset's price change in the last hour
-            asset_price_change_l1h = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price-change-h1']/span/text()").get()
-            asset_price_change_l1h = helper_normalize_numbers_in_pct_gains(value=asset_price_change_l1h)
+            asset_price_change_l1h_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price-change-h1']/span/text()").get()
+            asset_price_change_l1h = helper_normalize_numbers_in_pct_gains(value=asset_price_change_l1h_raw)
             
             # Extract the asset's price change in the last 6 hours
-            asset_price_change_l6h = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price-change-h6']/span/text()").get()
-            asset_price_change_l6h = helper_normalize_numbers_in_pct_gains(value=asset_price_change_l6h)
+            asset_price_change_l6h_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price-change-h6']/span/text()").get()
+            asset_price_change_l6h = helper_normalize_numbers_in_pct_gains(value=asset_price_change_l6h_raw)
             
             # Extract the asset's price change in the last 24 hours
-            asset_price_change_l24h = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price-change-h24']/span/text()").get()
-            asset_price_change_l24h = helper_normalize_numbers_in_pct_gains(value=asset_price_change_l24h)
+            asset_price_change_l24h_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-price-change-h24']/span/text()").get()
+            asset_price_change_l24h = helper_normalize_numbers_in_pct_gains(value=asset_price_change_l24h_raw)
             
             # Extract the asset's liquidity
-            asset_liquidity_in_mil = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-liquidity']/text()[2]").get()
-            asset_liquidity_in_mil = helper_normalize_numbers_in_vol_liq_mcap(value=asset_liquidity_in_mil)
+            asset_liquidity_in_mil_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-liquidity']/text()[2]").get()
+            asset_liquidity_in_mil = helper_normalize_numbers_in_vol_liq_mcap(value=asset_liquidity_in_mil_raw)
             
             # Extract the asset's market cap
-            asset_market_cap_in_mil = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-market-cap']/text()[2]").get()
-            asset_market_cap_in_mil = helper_normalize_numbers_in_vol_liq_mcap(value=asset_market_cap_in_mil)
+            asset_market_cap_in_mil_raw = res.xpath("./div[@class='ds-table-data-cell ds-dex-table-row-col-market-cap']/text()[2]").get()
+            asset_market_cap_in_mil = helper_normalize_numbers_in_vol_liq_mcap(value=asset_market_cap_in_mil_raw)
 
             # Yield the output dictionary
             output_dict = {
                 "asset_name": asset_name,
                 "asset_name_text": asset_name_text,
                 "asset_url": asset_url,
+                "asset_gain_rank_raw": asset_gain_rank_raw,
                 "asset_gain_rank": asset_gain_rank,
                 "asset_network": asset_network,
                 "dex": dex,
+                "asset_price_raw": asset_price_raw,
                 "asset_price": asset_price,
                 "asset_age": asset_age,
+                "asset_24_hr_txns_raw": asset_24_hr_txns_raw,
                 "asset_24_hr_txns": asset_24_hr_txns,
+                "asset_24_hr_volume_in_mil_raw": asset_24_hr_volume_in_mil_raw,
                 "asset_24_hr_volume_in_mil": asset_24_hr_volume_in_mil,
+                "num_makers_raw": num_makers_raw,
                 "num_makers": num_makers,
+                "asset_price_change_l5m_raw": asset_price_change_l5m_raw,
+                "asset_price_change_l1h_raw": asset_price_change_l1h_raw,
+                "asset_price_change_l6h_raw": asset_price_change_l6h_raw,
+                "asset_price_change_l24h_raw": asset_price_change_l24h_raw,
                 "asset_price_change_l5m": asset_price_change_l5m,
                 "asset_price_change_l1h": asset_price_change_l1h,
                 "asset_price_change_l6h": asset_price_change_l6h,
                 "asset_price_change_l24h": asset_price_change_l24h,
+                "asset_liquidity_in_mil_raw": asset_liquidity_in_mil_raw,
                 "asset_liquidity_in_mil": asset_liquidity_in_mil,
+                "asset_market_cap_in_mil_raw": asset_market_cap_in_mil_raw,
                 "asset_market_cap_in_mil": asset_market_cap_in_mil
             }
 
