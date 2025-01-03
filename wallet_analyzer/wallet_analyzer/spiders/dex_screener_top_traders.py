1ddca510783e350f337d240c3058834f1a36ddb8
Omar ElMaria <omar.elmaria@mim2018.esmt.org>
2025-01-03 14:51:58 +0200

    add raw cols to top traders crawler

--- wallet_analyzer/wallet_analyzer/spiders/dex_screener_top_traders.py
+++ wallet_analyzer/wallet_analyzer/spiders/dex_screener_top_traders.py
@@ -86,49 +86,62 @@ class DexScreenerTopTradersSpider(scrapy.Spider):
         # Parse the response
         for tr in top_trader_results:
             # Extract the trader bought amount in USD
-            trader_bought_usd = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/text()").get()
-            trader_bought_usd = helper_normalize_numbers_in_txn_data(value=trader_bought_usd)
+            trader_bought_usd_raw = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/text()").get()
+            trader_bought_usd = helper_normalize_numbers_in_txn_data(value=trader_bought_usd_raw)
 
             # Extract the trader bought amount in crypto units
-            trader_bought_crypto = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/following-sibling::span/span[1]/text()").get()
-            trader_bought_crypto = helper_normalize_numbers_in_txn_data(value=trader_bought_crypto)
+            trader_bought_crypto_raw = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/following-sibling::span/span[1]/text()").get()
+            trader_bought_crypto = helper_normalize_numbers_in_txn_data(value=trader_bought_crypto_raw)
             
             # Extract the number of buy TXNs
-            trader_buy_txns = helper_normalize_numbers_in_txn_data(value=tr.xpath(".//span[@class='chakra-text custom-rcecxm']/following-sibling::span/span[3]/text()").get())
+            trader_buy_txns_raw = tr.xpath(".//span[@class='chakra-text custom-rcecxm']/following-sibling::span/span[3]/text()").get()
+            trader_buy_txns = helper_normalize_numbers_in_txn_data(value=trader_buy_txns_raw)
             trader_buy_txns = helper_treat_none_before_data_type_change(value=trader_buy_txns, data_type="int")
 
             # Extract the trader sold amount in USD
-            trader_sold_usd = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/text()").get()
-            trader_sold_usd = helper_normalize_numbers_in_txn_data(value=trader_sold_usd)
+            trader_sold_usd_raw = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/text()").get()
+            trader_sold_usd = helper_normalize_numbers_in_txn_data(value=trader_sold_usd_raw)
             
             # Extract the trader sold amount in crypto units
-            trader_sold_crypto = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/following-sibling::span/span[1]/text()").get()
-            trader_sold_crypto = helper_normalize_numbers_in_txn_data(value=trader_sold_crypto)
+            trader_sold_crypto_raw = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/following-sibling::span/span[1]/text()").get()
+            trader_sold_crypto = helper_normalize_numbers_in_txn_data(value=trader_sold_crypto_raw)
             
             # Extract the number of sell TXNs
-            trader_sell_txns = helper_normalize_numbers_in_txn_data(value=tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/following-sibling::span/span[3]/text()").get())
+            trader_sell_txns_raw = tr.xpath(".//span[@class='chakra-text custom-dv3t8y']/following-sibling::span/span[3]/text()").get()
+            trader_sell_txns = helper_normalize_numbers_in_txn_data(value=trader_sell_txns_raw)
             trader_sell_txns = helper_treat_none_before_data_type_change(value=trader_sell_txns, data_type="int")
             
             # Extract the PnL
-            trader_pnl = helper_normalize_numbers_in_txn_data(value=tr.xpath(".//div[@class='custom-1e9y0rl']/text()").get())
-            trader_pnl = helper_normalize_numbers_in_txn_data(value=trader_pnl)
+            trader_pnl_raw = tr.xpath(".//div[@class='custom-1e9y0rl']/text()").get()
+            trader_pnl = helper_normalize_numbers_in_txn_data(value=trader_pnl_raw)
 
             # Extract the SOL scan URL
             sol_scan_url = tr.xpath(".//a[@aria-label='Open in block explorer']/@href").get()
 
             # Extract the wallet_address
-            wallet_address = re.findall(pattern=r"(?<=account\/).*", string=sol_scan_url)[0]
+            if sol_scan_url is not None:
+                wallet_address = re.findall(pattern=r"(?<=account\/).*", string=sol_scan_url)[0]
+            else:
+                wallet_address = None
 
             # Yield the output dictionary
             output_dict = {
                 "asset_name": asset_name,
                 "asset_url": asset_url,
+                "trader_bought_usd_raw": trader_bought_usd_raw,
                 "trader_bought_usd": trader_bought_usd,
+                "trader_bought_crypto_raw": trader_bought_crypto_raw,
                 "trader_bought_crypto": trader_bought_crypto,
+                "trader_buy_txns_raw": trader_buy_txns_raw,
                 "trader_buy_txns": trader_buy_txns,
+                "trader_sold_usd_raw": trader_sold_usd_raw,
+                "trader_sold_usd_raw": trader_sold_usd_raw,
                 "trader_sold_usd": trader_sold_usd,
+                "trader_sold_crypto_raw": trader_sold_crypto_raw,
                 "trader_sold_crypto": trader_sold_crypto,
+                "trader_sell_txns_raw": trader_sell_txns_raw,
                 "trader_sell_txns": trader_sell_txns,
+                "trader_pnl_raw": trader_pnl_raw,
                 "trader_pnl": trader_pnl,
                 "sol_scan_url": sol_scan_url,
                 "wallet_address": wallet_address
